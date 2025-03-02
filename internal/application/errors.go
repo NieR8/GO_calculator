dfc7ea59a594abcb33e4907bd16daa301535a663
Yabbadabbadoo <99741123+NieR8@users.noreply.github.com>
2025-03-02 11:43:28 +0700

    readme

--- internal/application/errors.go
+++
@@ -1,8 +0,0 @@
-package application
-
-import "errors"
-
-var (
-	ErrInvalidExprName = errors.New("в теле запроса ключ выражения должен называться expression либо выражение не должно быть пустым")
-	ErrInternalServer  = errors.New("internal server error")
-)
